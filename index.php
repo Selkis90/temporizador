8fcd3bd7877159875192b0c404d468c8a7958617
andres ramirez <programmer.arh@gmail.com>
2021-12-26 23:08:01 -0500

    Se suben todos los archios al repositorio

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="estilo/style.css?v=<?php echo time();?>">
+    </head>
+    <body>
+        <div id="contenedorCirculo">
+            <div id="contenedorHora">
+                <p id="hora">15:00</p>
+                <button id="star">STAR</button>
+            </div>
+            <div id="ruedaContenedor">
+            <img id="rueda"src="STARTER/images/gear.svg" alt="">
+            </div>
+        </div>
+    </body>
+</html>
